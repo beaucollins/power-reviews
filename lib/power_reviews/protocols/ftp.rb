5fb4fad94eda2ecf9554535ab20bf9618e5c1a44
Beau Collins <beaucollins@gmail.com>
2009-09-17 13:11:52 -0700

    implementing the ftp protocol

--- lib/power_reviews/protocols/ftp.rb
+++ lib/power_reviews/protocols/ftp.rb
@@ -1,6 +1,38 @@
+
+require "net/ftp"
+
 module PowerReviews
   module Protocols
     class Ftp < Base
+      
+      # Returns true if the done file is missing
+      def done?
+        !client.nlst.include?(@config['done'])
+      end
+      
+      # Delete the done file
+      def done!
+        client.delete(@config['done'])
+      end
+      
+      # Do the copying of the zip file to the preferred destination
+      def copy_zip(to)
+        puts 'FTP getting file'
+        client.getbinaryfile(@config['zip'], to)
+      end
+      
+      # do any cleanup necessary
+      def cleanup
+        @client.close
+      end
+      
+      def client
+        return @client unless @client.nil?
+        puts "Connecting to #{@config['host']} as #{@config['username']}"
+        @client = Net::FTP.new(@config['host'])
+        @client.login(@config['username'], @config['password'])
+        @client
+      end
       
     end
   end
